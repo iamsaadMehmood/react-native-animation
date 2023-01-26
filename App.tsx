01b97fc8bc30c7d6aaaea36c03cddfd67998a65d
Saad Mehmood <saad.mehmood@aquila360.com>
2023-01-26 23:51:08 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+import Feather from 'react-native-vector-icons/Feather';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Feather name="edit-2" size={24} color={'#000000'} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
